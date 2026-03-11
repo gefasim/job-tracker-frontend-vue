ece02a62f777b2d6ec355fdcb37fa7983f2085ec
Oleksandr Oliynyk <vov96now@gmail.com>
2026-03-11 18:45:41 +0200

    Move SVG from `JobTabContacts` into separate vue file

--- src/views/JobApplication/JobTabContacts.vue
+++ src/views/JobApplication/JobTabContacts.vue
@@ -4,6 +4,7 @@ import type { JobApplication } from '@/models/job-application.dto'
 import type { Contact } from '@/models/contact.dto'
 import ContactCard from './ContactCard.vue'
 import ContactModal from './ContactModal.vue'
+import ContactTabIcon from '@/assets/ContactTabIcon.vue'
 
 const jobApplication = defineModel<JobApplication>({ required: true })
 
@@ -52,22 +53,7 @@ const handleUnlinkContact = (contactId: string) => {
 
     <div v-if="!hasContacts" class="empty-state">
       <div class="empty-icon-wrapper">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-          <circle cx="9" cy="7" r="4" />
-          <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-          <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-        </svg>
+        <ContactTabIcon />
       </div>
       <h2>You have not linked any contacts to this job yet.</h2>
       <div class="empty-actions">
