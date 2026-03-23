183e21b9775063db2041e1219ba6d4d71128d744
Oleksandr Oliynyk <vov96now@gmail.com>
2026-03-23 20:52:14 +0200

    Show `UserBoards` component on Home page

--- src/views/AuthenticatedHome.vue
+++ src/views/AuthenticatedHome.vue
@@ -1,3 +1,48 @@
-<script setup lang="ts"></script>
-<template>AuthenticatedHome</template>
-<style></style>
+<script setup lang="ts">
+import UserBoards from './UserBoards.vue'
+</script>
+<template>
+  <div class="home">
+    <div class="left-sidebar">
+      <div>
+        <div>Contacts</div>
+        <div>Documents</div>
+        <hr />
+        Your boards:
+        <div>Board1</div>
+        <div>Board2</div>
+        <hr />
+        Help
+        <div>About</div>
+        <div>Contact Us</div>
+        <div>How to?</div>
+        <hr />
+      </div>
+      <div>
+        <div>Theme</div>
+        <div>LogOut</div>
+      </div>
+    </div>
+    <div class="main">
+      <UserBoards></UserBoards>
+    </div>
+  </div>
+</template>
+<style>
+.home {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  background: var(--bg-main);
+}
+.left-sidebar {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-right: 1px solid var(--border-color);
+}
+.main {
+  flex: 5;
+}
+</style>
--- src/views/UserBoards.vue
+++ src/views/UserBoards.vue
@@ -1,29 +1,82 @@
 <script setup lang="ts">
 import { api } from '@/api/api'
-import { CurrentUserService } from '@/current-user.service'
+import type { Board } from '@/models/board.dto'
 import { onMounted, ref } from 'vue'
-import { RouterLink } from 'vue-router'
+import { useRouter } from 'vue-router'
 
-const boards = ref()
-const user = CurrentUserService.getUser()
+const router = useRouter()
+const boards = ref<Board[]>()
 
 const fetchData = async () => {
   const response = await api.boards.getAll()
-  boards.value = response
+  boards.value = response.filter((b) => !b.isArchived)
 }
 
 onMounted(() => {
   fetchData()
 })
+
+const openBoard = (boardId: string) => {
+  router.push({ name: 'board', params: { boardId } })
+}
 </script>
 
 <template>
-  <h1>User's boards:</h1>
-  <p>User firstName: {{ user?.firstName ?? 'null' }}</p>
-  <div v-for="board in boards" :key="board.name">
-    board name: {{ board.name }},
-    <RouterLink :to="{ name: 'board', params: { boardId: board.id } }">{{ board.id }}</RouterLink>
+  <div class="boards-view">
+    <div class="boards-view-header">
+      <h3>My Job Tracking Boards</h3>
+      <p class="boards-view-archived">view archived</p>
+      <hr />
+    </div>
+
+    <div class="board-card-grid">
+      <div
+        v-for="board in boards"
+        :key="board.name"
+        class="board-card"
+        @click="openBoard(board.id)"
+      >
+        <span class="board-card-title">
+          {{ board.name }}
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.boards-view {
+  padding: 10%;
+}
+.boards-view-header {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+.boards-view-archived {
+  font-weight: 400;
+  border: 1px dashed var(--border-color);
+  border-radius: 8px;
+  padding: 0.5rem;
+  cursor: pointer;
+}
+.board-card-grid {
+  margin-top: 2rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 1rem;
+}
+.board-card {
+  background: var(--bg-card);
+  padding: 1rem;
+  border: 1px solid var(--border-color);
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+}
+.board-card-title {
+  font-size: 1.25rem;
+  font-weight: 600;
+}
+</style>
