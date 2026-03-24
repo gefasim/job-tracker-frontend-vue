35ac4900b9b10ec8c0f782cbfc06fd1dc3cc5b2a
Oleksandr Oliynyk <vov96now@gmail.com>
2026-03-24 19:40:47 +0200

    Show active button on LeftSidebar

--- src/views/LeftSidebar.vue
+++ src/views/LeftSidebar.vue
@@ -8,8 +8,10 @@ import HowToIcon from '@/assets/icons/HowToIcon.vue'
 import LightThemeIcon from '@/LightThemeIcon.vue'
 import DarkThemeIcon from '@/assets/icons/DarkThemeIcon.vue'
 import { useTheme, type ColorScheme } from '@/store/themeStore'
+import { ref } from 'vue'
 
 const { isDark, theme, setTheme } = useTheme()
+const activeItem = ref<string>('contacts')
 
 const switchTheme = () => {
   const themeOptions: Array<ColorScheme> = ['light', 'dark', 'system']
@@ -17,21 +19,67 @@ const switchTheme = () => {
   const nextIndex = (currentIndex + 1) % themeOptions.length
   setTheme(themeOptions[nextIndex]!)
 }
+
+const handleItemClick = (item: string) => {
+  activeItem.value = item
+}
 </script>
 <template>
   <div class="left-sidebar">
     <div>
-      <div class="sidebar-item"><ContactNavTabIcon /> Contacts</div>
-      <div class="sidebar-item"><DocumentNavTabIcon /> Documents</div>
+      <div
+        class="sidebar-item"
+        :class="{ active: activeItem === 'contacts' }"
+        @click="handleItemClick('contacts')"
+      >
+        <ContactNavTabIcon /> Contacts
+      </div>
+      <div
+        class="sidebar-item"
+        :class="{ active: activeItem === 'documents' }"
+        @click="handleItemClick('documents')"
+      >
+        <DocumentNavTabIcon /> Documents
+      </div>
       <hr />
       Your boards:
-      <div class="sidebar-item"><BoardIcon /> Board1</div>
-      <div class="sidebar-item"><BoardIcon /> Board2</div>
+      <div
+        class="sidebar-item"
+        :class="{ active: activeItem === 'board1' }"
+        @click="handleItemClick('board1')"
+      >
+        <BoardIcon /> Board1
+      </div>
+      <div
+        class="sidebar-item"
+        :class="{ active: activeItem === 'board2' }"
+        @click="handleItemClick('board2')"
+      >
+        <BoardIcon /> Board2
+      </div>
       <hr />
       Help
-      <div class="sidebar-item"><AboutIcon /> About</div>
-      <div class="sidebar-item"><ContactUsIcon /> Contact Us</div>
-      <div class="sidebar-item"><HowToIcon /> How to?</div>
+      <div
+        class="sidebar-item"
+        :class="{ active: activeItem === 'about' }"
+        @click="handleItemClick('about')"
+      >
+        <AboutIcon /> About
+      </div>
+      <div
+        class="sidebar-item"
+        :class="{ active: activeItem === 'contact-us' }"
+        @click="handleItemClick('contact-us')"
+      >
+        <ContactUsIcon /> Contact Us
+      </div>
+      <div
+        class="sidebar-item"
+        :class="{ active: activeItem === 'how-to' }"
+        @click="handleItemClick('how-to')"
+      >
+        <HowToIcon /> How to?
+      </div>
       <hr />
     </div>
     <div>
@@ -40,8 +88,20 @@ const switchTheme = () => {
         <span>Theme</span>
         <span class="theme-value">({{ theme }})</span>
       </div>
-      <div class="sidebar-item">Personal Account Settings</div>
-      <div class="sidebar-item">Log Out</div>
+      <div
+        class="sidebar-item"
+        :class="{ active: activeItem === 'settings' }"
+        @click="handleItemClick('settings')"
+      >
+        Personal Account Settings
+      </div>
+      <div
+        class="sidebar-item"
+        :class="{ active: activeItem === 'logout' }"
+        @click="handleItemClick('logout')"
+      >
+        Log Out
+      </div>
     </div>
   </div>
 </template>
@@ -83,5 +143,10 @@ html.dark .sidebar-item:hover {
 
 .theme-switch:hover .theme-value {
   opacity: 1;
+}
+
+.sidebar-item.active {
+  border: 1px solid #1a73e8;
+  background-color: #2563eb66;
 }
 </style>
