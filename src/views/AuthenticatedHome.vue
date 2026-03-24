8cacde6fc74d409d993676e7ac1a5be1f7e3bbf0
Oleksandr Oliynyk <vov96now@gmail.com>
2026-03-24 17:35:34 +0200

    Create LeftSidebar as separate component

--- src/views/AuthenticatedHome.vue
+++ src/views/AuthenticatedHome.vue
@@ -1,26 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LeftSidebar from '@/views/LeftSidebar.vue'
+</script>
 <template>
   <div class="home">
-    <div class="left-sidebar">
-      <div>
-        <div>Contacts</div>
-        <div>Documents</div>
-        <hr />
-        Your boards:
-        <div>Board1</div>
-        <div>Board2</div>
-        <hr />
-        Help
-        <div>About</div>
-        <div>Contact Us</div>
-        <div>How to?</div>
-        <hr />
-      </div>
-      <div>
-        <div>Theme</div>
-        <div>LogOut</div>
-      </div>
-    </div>
+    <LeftSidebar />
     <main class="main-content">
       <RouterView />
     </main>
@@ -32,14 +15,6 @@
   flex-direction: row;
   height: 100vh;
   background: var(--bg-main);
-}
-.left-sidebar {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  border-right: 1px solid var(--border-color);
-  min-width: 10vw;
 }
 .main-content {
   flex: 5;
---
+++ src/views/LeftSidebar.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import AboutIcon from '@/assets/AboutIcon.vue'
+import BoardIcon from '@/assets/BoardIcon.vue'
+import ContactNavTabIcon from '@/assets/ContactNavTabIcon.vue'
+import DocumentNavTabIcon from '@/assets/DocumentNavTabIcon.vue'
+import ContactUsIcon from '@/assets/ContactUsIcon.vue'
+import HowToIcon from '@/assets/HowToIcon.vue'
+import LightThemeIcon from '@/LightThemeIcon.vue'
+import DarkThemeIcon from '@/assets/DarkThemeIcon.vue'
+</script>
+<template>
+  <div class="left-sidebar">
+    <div>
+      <div><ContactNavTabIcon /> Contacts</div>
+      <div><DocumentNavTabIcon /> Documents</div>
+      <hr />
+      Your boards:
+      <div><BoardIcon /> Board1</div>
+      <div><BoardIcon /> Board2</div>
+      <hr />
+      Help
+      <div><AboutIcon /> About</div>
+      <div><ContactUsIcon /> Contact Us</div>
+      <div><HowToIcon /> How to?</div>
+      <hr />
+    </div>
+    <div>
+      <div><LightThemeIcon /> <DarkThemeIcon /> Theme</div>
+      <div>Personal Account Settings</div>
+      <div>Log Out</div>
+    </div>
+  </div>
+</template>
+<style>
+.left-sidebar {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-right: 1px solid var(--border-color);
+  min-width: 10vw;
+}
+</style>
