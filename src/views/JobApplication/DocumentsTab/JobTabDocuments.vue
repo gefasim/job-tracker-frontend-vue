5c65880dfe544f27b9dd8eb178e6141330d60295
Oleksandr Oliynyk <vov96now@gmail.com>
2026-03-26 20:21:54 +0200

    Implement DocumentGrid

--- src/views/JobApplication/DocumentsTab/JobTabDocuments.vue
+++ src/views/JobApplication/DocumentsTab/JobTabDocuments.vue
@@ -2,21 +2,21 @@
 import { ref, computed, onMounted } from 'vue'
 import type { JobApplication } from '@/models/job-application.dto'
 import type { Document } from '@/models/document.dto'
-import DocumentsCard from './DocumentsCard.vue'
 import DocumentModal from './DocumentModal.vue'
 import LinkDocumentDropdown from '@/views/Shared/LinkDocumentDropdown.vue'
 import { api } from '@/api/api'
 import { useCurrentBoard } from '@/store/currentBoardStore'
+import DocumentGrid from './DocumentGrid.vue'
+import { useRoute } from 'vue-router'
 
 const jobApplication = defineModel<JobApplication>({ required: true })
 const { board } = useCurrentBoard()
+const route = useRoute()
+const boardId = route.params.boardId as string
 
 const isModalOpen = ref(false)
 const documentToEdit = ref<Document | null>(null)
 
-const hasDocuments = computed(() => {
-  return jobApplication.value.documents && jobApplication.value.documents.length > 0
-})
 const boardDocuments = ref<Document[]>([])
 const availableDocumentsToLink = computed(() => {
   const assignedDocumentIds = jobApplication.value.documents?.map((c) => c.id)
@@ -30,11 +30,6 @@ onMounted(async () => {
 
 const openUploadModal = () => {
   documentToEdit.value = null
-  isModalOpen.value = true
-}
-
-const openEditModal = (document: Document) => {
-  documentToEdit.value = JSON.parse(JSON.stringify(document))
   isModalOpen.value = true
 }
 
@@ -77,38 +72,16 @@ const handleUnlinkDocument = async (documentId: string) => {
       </div>
     </div>
 
-    <div v-if="!hasDocuments" class="empty-state">
-      <div class="empty-icon-wrapper">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-          <polyline points="14 2 14 8 20 8"></polyline>
-          <line x1="16" y1="13" x2="8" y2="13"></line>
-          <line x1="16" y1="17" x2="8" y2="17"></line>
-          <polyline points="10 9 9 9 8 9"></polyline>
-        </svg>
-      </div>
-      <h2>You have not linked any documents to this job yet.</h2>
-    </div>
-
-    <div v-else class="documents-grid">
-      <DocumentsCard
-        v-for="doc in jobApplication.documents"
-        :key="doc.id"
-        :document="doc"
-        @edit="openEditModal"
-        @unlink="handleUnlinkDocument"
-      />
-    </div>
+    <DocumentGrid
+      :documents="jobApplication.documents"
+      :board-id="boardId"
+      :job-application="jobApplication"
+      no-documents-message="You have not linked any documents to this job yet."
+      :show-unlink-button="true"
+      :show-delete-button="false"
+      @unlink="handleUnlinkDocument"
+      @save="handleSaveDocument"
+    />
 
     <Teleport to="body">
       <DocumentModal
